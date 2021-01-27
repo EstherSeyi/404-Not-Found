88cd33a8bbc153498aafe6e4960ae8354ceee6ea
EstherSeyi <sheyiogundijo@gmail.com>
2021-01-27 14:45:45 +0100

    modifies not found page

--- src/components/not-found/index.tsx
+++ src/components/not-found/index.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 import scareCrow from "../../assets/Scarecrow.png";
 
 const NotFound = () => {
   return (
-    <section className="w-11/12 mx-auto my-0 min-h-screen h-screen relative">
+    <section className="w-11/12 mx-auto my-0 min-h-screen relative">
       <h1 className="text-dark uppercase font-medium text-24 mt-2.5 font-inconsolata mb-16 sm:mb-48">
         404 not found
       </h1>
       <div className="sm:flex justify-between">
-        <div className=" mb-10 flex-35 mr-28">
-          <img src={scareCrow} alt="scarecrow" />
+        <div className=" mb-10 flex-35 sm:mr-28">
+          <img className="w-full" src={scareCrow} alt="scarecrow" />
         </div>
         <div className="flex-35">
           <p className="font-spaceMono text-38 font-bold leading-50 mb-6 lg:w-9/12">
@@ -18,13 +20,16 @@ const NotFound = () => {
             The page you are looking for might be removed or is temporarily
             unavailable
           </p>
-          <button className="bg-dark text-white py-3 px-5 text-bold uppercase sm:text-14">
+          <Link
+            to="/"
+            className="bg-dark text-white py-3 px-5 text-bold uppercase sm:text-14"
+          >
             Back to homepage
-          </button>
+          </Link>
         </div>
       </div>
 
-      <p className="absolute top-95% text-14 w-full text-center font-bold">
+      <p className="absolute top-full sm:top-95% text-14 w-full text-center font-bold">
         Esther @ DevChallenges.io
       </p>
     </section>
